dd61a4166d977f24f0e66f00cecdcc75bf328c9a
Bruno Meilick <b@bnomei.com>
2018-10-10 09:34:10 +0200

    rss fix

--- snippets/feed/rss.php
+++ snippets/feed/rss.php
@@ -1,5 +1,7 @@
-<?php use \Kirby\Toolkit\XML; ?><?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
+<?php use \Kirby\Toolkit\XML;
+
+echo '<?xml version="1.0" encoding="utf-8"?>';
+?><rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
 
   <channel>
     <title><?= XML::encode($title) ?></title>
