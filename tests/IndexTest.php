97ed35e966f9af2b55b5282e39bcbbc5148b3f33
Bruno Meilick <b@bnomei.com>
2019-08-20 11:47:32 +0200

    feed routes test
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -13,28 +13,28 @@ class IndexTest extends TestCase
     public function testFindsHomePage()
     {
         $response = kirby()->render('/');
-        $this->assertIsInt($response->code(), 200);
+        $this->assertTrue($response->code() === 200);
         $this->assertStringContainsString('Home', $response->body());
     }
 
     public function testFindsFeedRoute()
     {
         $response = kirby()->render('/feed');
-        $this->assertIsInt($response->code(), 200);
+        $this->assertTrue($response->code() === 200);
         $this->assertTrue('application/rss+xml' === $response->type());
     }
 
     public function testFindsFeedRouteJSON()
     {
         $response = kirby()->render('/feed-json');
-        $this->assertIsInt($response->code(), 200);
+        $this->assertTrue($response->code() === 200);
         $this->assertTrue('application/json' === $response->type());
     }
 
     public function testFindsFeedRouteYAML()
     {
         $response = kirby()->render('/feed-yaml');
-        $this->assertIsInt($response->code(), 200);
+        $this->assertTrue($response->code() === 200);
         $this->assertTrue('text/html' === $response->type());
     }
 }
