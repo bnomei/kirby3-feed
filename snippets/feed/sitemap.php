c2668c2a79186779dd7834887596298f94c7874b
Bruno Meilick <b@bnomei.com>
2021-08-14 17:47:45 +0100

    :bug: sitemap as snippet not working with all servers

--- snippets/feed/sitemap.php
+++ snippets/feed/sitemap.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?= '<?xml version="1.0" encoding="utf-8"?>' ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml"
         <?php if($images): ?>xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"<?php endif; ?>
